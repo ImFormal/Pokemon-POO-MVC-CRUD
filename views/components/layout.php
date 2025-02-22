4c8abed10a0ba9a4861a4e0fda9b1ff661b78828
ImFormal <thubert.maxime.pro@gmail.com>
2025-02-22 18:28:48 +0100

    update

--- views/components/layout.php
+++ views/components/layout.php
@@ -6,6 +6,7 @@
         <meta name="description" content=<?= $description ?>>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
         <link rel="stylesheet" href="public/style/style.css">
+        <link rel="icon" type="image/png" href="public/images/icone/pokemon.png">
         <title><?= $title ?></title>
     </head>
     
